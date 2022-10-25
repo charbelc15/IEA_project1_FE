4d991a22820c9a95173ecb18a0b94174f35c338c
charbelc15 <charbelc15@gmail.com>
2022-10-26 02:27:36 +0300

    Adding files

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import DrawingCanvas from './components/DrawingCanvas/DrawingCanvas';
+
+function App() {
+  return (
+    <DrawingCanvas></DrawingCanvas>
+  );
+}
+
+export default App;
