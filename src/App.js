7fdb4fdd81a8bebca6eb42c24df5320122fdf707
charbelc15 <charbelc15@gmail.com>
2022-11-02 20:47:33 +0200

    Images and Two Models added

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
 
 import './App.css';
-import React, { useState, useRef } from 'react';
 import DrawingCanvas from './components/DrawingCanvas/DrawingCanvas';
-import PredictButton from './components/Predict/PredictButton';
-import RetrainButton from './components/Retrain/RetrainButton';
+import Model1 from './components/Model1/Model1';
+import Model2 from './components/Model2/Model2';
 import TranslateButton from './components/Translate/TranslateButton';
 import axios from "axios";
 
 function App() {
 
-  function sayHello() {
-    axios({
-      method: 'get',
-      url: 'http://localhost:8000/printme',
-      headers: {
-          'Accept': 'application/json'
-          },
-      })
-      .then(function(response) {
-          alert(response.data.success)
-      })
-      .catch(function(response) {
-          console.error(response);
-      });
-  } 
+  // function sayHello() {
+  //   axios({
+  //     method: 'get',
+  //     url: 'http://localhost:8000/printme',
+  //     headers: {
+  //         'Accept': 'application/json'
+  //         },
+  //     })
+  //     .then(function(response) {
+  //         alert(response.data.success)
+  //     })
+  //     .catch(function(response) {
+  //         console.error(response);
+  //     });
+  // } 
 
   function translate() {
     var textToTranslate = document.getElementById("toTranslate").value;
@@ -42,71 +41,6 @@ function App() {
       });
   } 
 
-  const [file, setFile] = useState([]);
-  const [file2, setFile2] = useState([]);
-  const RetrainLabel = useRef()
- 
-  const predict = e => {
-    console.log(e.target.files[0])
-    // setFile([...file, e.target.files[0]]);
-
-    let formData = new FormData();
-    formData.append('file', e.target.files[0]);
-    formData.append('type', e.target.files[0].type);
-    
-    axios.post('http://localhost:8000/preprocess_inference', formData, {
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'multipart/form-data'
-        }
-      })
-      .then(function(response) {
-        document.getElementById("label").value="Image label:" + response.data.data;
-        document.getElementById("toTranslate").value= document.getElementById("toTranslate").value + response.data.data[2];
-        console.log(response.data.data)
-      })      
-      .catch(function(response) {
-        document.getElementById("label").value="cant display file label";
-        console.error(response);
-      });
-    e.preventDefault();
-  }
-
-  const retrain = e => {
-    // setFile([...file2, e.target.files[0]]);
-
-    let formData = new FormData();
-    formData.append('image', e.target.files[0]);      // To change key according to anthony param
-    formData.append('type', e.target.files[0].type);
-
-    var label = RetrainLabel.current.value;
-
-    console.log(label.length)
-    
-    if(label.length>0){
-      
-      axios.post('http://localhost:8000/re-train?output_label='+label, formData, {
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'multipart/form-data'
-        }
-      })
-      .then(function(response) {
-        alert("Retrain Done! and " + label + " was added")
-        console.log(response.data.success);
-      })      
-      .catch(function(response) {
-        alert("Retrain fail");
-      });
-    
-    }
-    else{
-      alert("Fill Label output value!")
-    }
-    
-      e.preventDefault();
-    
-  }
 
   return (
     < >
@@ -116,51 +50,31 @@ function App() {
       </div>
 
       <div className='App'>
-        <label>
-          PREDICTION
-        </label>
-      </div>
-      
-      <div className='App'>  
-        <PredictButton onClick={() => { }}>
-        <input type="file" onChange={predict} ></input>
-        </PredictButton>
-
-        <textarea id='label' placeholder="Label" disabled readOnly></textarea>
+        <Model1> 
+        </Model1>
       </div>
 
       <div className='App'>
-        <label>
-          RETRAIN
-        </label>
+        <Model2> 
+        </Model2>
       </div>
-      
-      <div className='App'>
-        <label>
-              Retrain Label:
-              <input required ref={RetrainLabel} id="retrainlabel" type="text"  />
-        </label>
 
-        <RetrainButton onClick={() => { }}>
-          Retrain--
-        <input type="file" onChange={retrain} ></input>
-        </RetrainButton>
+
+      <div className='App'>
+        <h2 id="Translationlabel">
+        ------------------------------------------------------ TRANSLATION---------------------------------------------------------
+        </h2>
       </div>
 
       <div className='App'>
-        <label>
-          TRANSLATION
-        </label>
-      </div>
-
-      <div className='App'>
-        <textarea id='toTranslate' placeholder="Text to translate" disabled readOnly></textarea>
+        <div className='warning'>
+        <textarea id='toTranslate' placeholder="Text to translate"></textarea>
         <TranslateButton onClick={translate}>
           Translate
         </TranslateButton>
         <textarea id='translated' placeholder="Translation output" disabled readOnly></textarea>
+        </div>
       </div>
-
     </>
   );
 }
---
+++ src/components/Model2/Model2.js
@@ -0,0 +1,197 @@
+import React, { useState, useRef } from 'react';
+import PredictButton from '../Predict/PredictButton';
+import RetrainButton from '../Retrain/RetrainButton';
+import axios from "axios";
+import './Model2.css'
+
+
+
+const Model2 = () => {
+    
+    const [BB_image, setBBImage] = useState("");
+    const [Cropped_image, set_Cropped_Image] = useState("");
+    const [Delated_image, set_Delated_Image] = useState("");
+    const [Resized_image, set_Resized_Image] = useState("");
+
+    const RetrainLabel = useRef()
+
+    const predict = e => {
+    console.log("Uploaded File: ",e.target.files[0])
+    // setFile([...file, e.target.files[0]]);
+
+    let formData = new FormData();
+    formData.append('file', e.target.files[0]);
+    formData.append('type', e.target.files[0].type);
+    
+    axios.post('http://localhost:8000/preprocess_inference_model2', formData, {
+        headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'multipart/form-data'
+        }
+        })
+        .then(function(response) {
+        getBBImage()
+        getCroppedImage()
+        getDelatedImage()
+        getResizedImage()
+        console.log("FastAPI model2 response", response)
+        // response.data.data="['A']"
+        document.getElementById("ImagelabelModel2").value="Image label:" + response.data.data;
+        document.getElementById("toTranslate").value= document.getElementById("toTranslate").value + response.data.data[2];
+        console.log(response.data.data)
+        })      
+        .catch(function(response) {
+        document.getElementById("ImagelabelModel2").value="cant display file label";
+        console.error(response);
+        });
+    e.preventDefault();
+    }
+
+    // const retrain = e => {
+    // // setFile([...file2, e.target.files[0]]);
+
+    // let formData = new FormData();
+    // formData.append('image', e.target.files[0]);      // To change key according to anthony param
+    // formData.append('type', e.target.files[0].type);
+
+    // var label = RetrainLabel.current.value;
+
+    // console.log(label.length)
+    
+    // if(label.length>0){
+        
+    //     axios.post('http://localhost:8000/re-train?output_label='+label, formData, {
+    //     headers: {
+    //     'Accept': 'application/json',
+    //     'Content-Type': 'multipart/form-data'
+    //     }
+    //     })
+    //     .then(function(response) {
+    //     alert("Retrain Done! and " + label + " was added")
+    //     console.log(response.data.success);
+    //     })      
+    //     .catch(function(response) {
+    //     alert("Retrain fail");
+    //     });
+    
+    // }
+    // else{
+    //     alert("Fill Label output value!")
+    // }
+    
+    //     e.preventDefault();
+    
+    // }
+
+    const getBBImage = () => {
+    axios.get("http://localhost:8000/send_BB_Image", {
+        'accept': 'application/json'
+        })
+        .then((res) => {
+        console.log(res.data.data )
+        const base64 = res.data.data
+        setBBImage(base64)
+    })
+    }
+
+    const getCroppedImage = () => {
+        axios.get("http://localhost:8000/send_Cropped_Image", {
+            'accept': 'application/json'
+            })
+            .then((res) => {
+            console.log(res.data.data )
+            const base64 = res.data.data
+            set_Cropped_Image(base64)
+    })
+    }
+    
+    const getDelatedImage = () => {
+        axios.get("http://localhost:8000/send_Delated_Image", {
+            'accept': 'application/json'
+            })
+            .then((res) => {
+            console.log(res.data.data )
+            const base64 = res.data.data
+            set_Delated_Image(base64)
+    })
+    }
+
+    const getResizedImage = () => {
+        axios.get("http://localhost:8000/send_Resized_Image", {
+            'accept': 'application/json'
+            })
+            .then((res) => {
+            console.log(res.data.data )
+            const base64 = res.data.data
+            set_Resized_Image(base64)
+    })
+    }
+
+    return(
+    <div>
+        
+        <div className='App'>
+            <h2 id="ModelLabel">
+        ------------------------------MODEL 2---------------------------------
+            </h2>
+        </div>
+        
+        
+        <div className='App'>
+            <h3 id="PredictionLabel">
+            -------------------------- PREDICTION --------------------------
+            </h3>
+        </div>
+      
+        <div className='warning'>  
+            <PredictButton onClick={() => { }}>
+            <input type="file" onChange={predict} ></input>
+            </PredictButton>
+
+            <textarea id='ImagelabelModel2' placeholder="Model 2 Label" disabled readOnly></textarea>
+        </div>
+      
+        <div className='App'>
+            <label id="BoundingBoxLabel"> Bounded Box: </label>
+            <img id="image" src={`data:image/png;base64,${BB_image}`} alt="" />
+            <label id="BoundingBoxLabel"> ----------> </label>
+
+            <label id="BoundingBoxLabel"> Cropped Image: </label>
+            <img id="image" src={`data:image/png;base64,${Cropped_image}`} alt="" />
+            <label id="BoundingBoxLabel"> ----------> </label>
+
+            <label id="BoundingBoxLabel"> Delated Image: </label>
+            <img id="image" src={`data:image/png;base64,${Delated_image}`} alt="" />
+            <label id="BoundingBoxLabel"> ----------> </label>
+
+            <label id="BoundingBoxLabel"> Resized Image: </label>
+            <img id="image" src={`data:image/png;base64,${Resized_image}`} alt="" />
+
+        </div>
+
+        
+        
+        {/* <div className='App'>
+            <label  id="RetrainLabel">
+            -------------------------- RETRAIN --------------------------
+            </label>
+        </div>
+      
+        <div className='App'>
+            <label>
+                Retrain Label:
+                <input required ref={RetrainLabel} id="retrainlabel" type="text"  />
+            </label>
+
+            <RetrainButton onClick={() => { }}>
+            <input type="file" onChange={retrain} ></input>
+            </RetrainButton>
+        </div>
+      */}
+
+    </div>
+     
+    )
+}
+
+export default Model2;
